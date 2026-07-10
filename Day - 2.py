03293678cdcb42ae63bdc4380ad9d9dc029d6b16
mubeenacodes07 <shaik.mubeena0450@gmail.com>
2026-07-10 16:27:04 +0530

    Create Day - 2.py
    Day 2 completed.

---
+++ Day - 2.py
@@ -0,0 +1,6 @@
+name = input("What is your name?")
+print("Hello" + name + "! Welcome to day 2")
+
+age = input("What is your age?")
+print("Wow, you are " + age + "years old!")
+print("You are doing great in coding!")
