2ff6aed18ae7c23a9f4720e4000e871ecbddef8e
mubeenacodes07 <shaik.mubeena0450@gmail.com>
2026-07-13 16:22:42 +0530

    Create Day-5code -dictionary_demo.py
    Python_Dictionary/student_info.py

---
+++ Day-5code -dictionary_demo.py
@@ -0,0 +1,10 @@
+student = {
+  "name": "Mubeena",
+  "course": "BCA",
+  "language": "python",
+  "day": 5
+}
+print("student Info:", student)
+print("Name is:", student["name"])
+print["topic"] = "Dictionary"
+print("Updated Info:", student)
